c25e3cab236866794705b0021a89d4a1a6e2beb7
Slash.Huang <slash.huang@tw.fujitsu.com>
2015-11-20 17:27:55 +0800

    update set_arm_4412_toolchain_path.sh

--- set_arm_4412_toolchain_path.sh
+++ set_arm_4412_toolchain_path.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PATH=$PATH:/home/slash/tiny4412/opt/FriendlyARM/toolschain/4.5.1/bin
+PATH=$PATH:/home/cadtc/tiny4412/opt/FriendlyARM/toolschain/4.5.1/bin
 
 export CROSS_COMPILE=arm-none-linux-gnueabi-
 export ARCH=arm
